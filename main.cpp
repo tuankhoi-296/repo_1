fed1c27f422d5f3afd9d86a48aaa0dfd975b69bb
tuankhoi-296 <tuankhoile020906@gmail.com>
2025-09-21 17:07:34 +0700

    first commit

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+int main() {
+    std::cout << "Hello, World CPP!" << std::endl;
+    cout<<"Tao la 1 con cho"<<endl;
+    cout<<"hello tui may"<<endl;
+    return 0;
+}
---
+++ test.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    cout<<"Hello tao la Tuan Khoi"<<endl;
+    cout<<"tao la so 1"<<endl;
+    cout<<"tao la so 2"<<endl;
+    int n;
+    cin>>n;
+    cout<<n+1<<endl;
+    vector<int> a(n);
+    for(int i=0;i<n;i++) cin>>a[i]
+    for (size_t i = 0; i < n; i++)
+    {
+            cout<<a[i]<<" ";    
+    
+    }
+    return 0;
+}
